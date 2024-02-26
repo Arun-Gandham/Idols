03ffd28bdbadd7d36c553f1c89ac4fab6acc58c4
arunsai.gandham <arunsai.gandham@karkinos.in>
2024-02-26 23:47:54 +0530

    Added upload image field with image privew

--- resources/views/templates/pages/forms/admin_form.blade.php
+++ resources/views/templates/pages/forms/admin_form.blade.php
@@ -2,34 +2,69 @@
 
 @section('title', 'DataTables - Tables')
 
-@section('vendor-style')
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/flatpickr/flatpickr.css') }}" />
-    <!-- Row Group CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-rowgroup-bs5/rowgroup.bootstrap5.css') }}">
-    <!-- Form Validation -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/@form-validation/umd/styles/index.min.css') }}" />
-@endsection
-
-@section('vendor-script')
-    <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
-    <!-- Flat Picker -->
-    <script src="{{ asset('assets/vendor/libs/moment/moment.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/flatpickr/flatpickr.js') }}"></script>
-    <!-- Form Validation -->
-    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/bundle/popular.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/plugin-bootstrap5/index.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js') }}"></script>
-@endsection
-
-@section('page-script')
-    {{-- <script src="{{ asset('assets/js/tables-datatables-basic.js') }}"></script> --}}
-@endsection
-
 @section('content')
+
+    <style>
+        .button {
+            display: inline-block;
+            background-color: #007bff;
+            color: #fff;
+            padding: 10px 20px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+
+        .image-preview {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .preview-image {
+            max-width: 100%;
+            max-height: 200px;
+            margin-bottom: 10px;
+        }
+
+        .remove-button {
+            background-color: #dc3545;
+            color: #fff;
+            border: none;
+            padding: 8px 15px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+
+        .image-outer {
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            border: 2px dashed #dbdade;
+            border-radius: 0.5rem;
+            width: 13.25rem;
+            margin: 0px auto
+        }
+
+        .image-outer img {
+            max-height: 100%;
+            max-width: 100%;
+        }
+
+        .removeButton {
+            color: #6f6b7d;
+            border-top: 1px solid #dbdade;
+            border-bottom-right-radius: calc(0.375rem - 1px);
+            border-bottom-left-radius: calc(0.375rem - 1px);
+            display: block;
+            text-align: center;
+            padding: 0.375rem 0;
+            font-size: 0.75rem;
+            width: 80%;
+            margin: 10px;
+            background: #fff;
+        }
+    </style>
     @if ($error = session('error'))
         <div class="alert alert-danger d-flex align-items-center alert-dismissible" role="alert">
             {{ $error }}
@@ -53,45 +88,80 @@
                 <form class="card-body" method="POST"
                     action="{{ isset($user) ? route('admin.edit.submit') : route('admin.add.submit') }}">
                     @csrf
-                    <div class="row mb-3">
-                        <div class="col-md-6">
-                            <label class="col-sm-3 col-form-label" for="multicol-username">User Name</label>
-                            <div class="col-sm-9">
-                                <input type="text" id="multicol-username" class="form-control" placeholder="User Name"
-                                    name="username" value="{{ isset($user) ? $user->name : '' }}" required>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="image-outer">
+                                <label for="imageUpload" id="uploadButton" class="button">Profile Picture</label>
+                                <input type="file" id="imageUpload" accept="image/*" style="display: none;" required>
+                                <div id="imagePreview" class="image-preview"></div>
                             </div>
                         </div>
+                        <div class="col-md-9">
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <label class="col-sm-3 col-form-label" for="multicol-username">User Name</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" id="multicol-username" class="form-control"
+                                            placeholder="User Name" name="username"
+                                            value="{{ isset($user) ? $user->name : '' }}" required>
+                                    </div>
+                                </div>
 
-                        <div class="col-md-6">
-                            <label class="col-sm-3 col-form-label" for="multicol-username">Email</label>
-                            <div class="col-sm-9">
-                                <input type="email" id="multicol-username" class="form-control" placeholder="Email"
-                                    name="email" value="{{ isset($user) ? $user->email : '' }}" required>
-                            </div>
-                        </div>
+                                <div class="col-md-6">
+                                    <label class="col-sm-3 col-form-label" for="multicol-username">Email</label>
+                                    <div class="col-sm-9">
+                                        <input type="email" id="multicol-username" class="form-control"
+                                            placeholder="Email" name="email"
+                                            value="{{ isset($user) ? $user->email : '' }}" required>
+                                    </div>
+                                </div>
 
-                        <div class="col-md-6">
-                            <label class="col-sm-3 col-form-label" for="multicol-username">Password</label>
-                            <div class="col-sm-9">
-                                <input type="hidden" name="id" value="{{ isset($user) ? $user->id : '' }}">
-                                <input type="text" id="multicol-username" class="form-control" placeholder="password"
-                                    name="password" {{ isset($user) ? '' : 'required' }}>
-                            </div>
-                        </div>
+                                <div class="col-md-6">
+                                    <label class="col-sm-3 col-form-label" for="multicol-username">Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="hidden" name="id" value="{{ isset($user) ? $user->id : '' }}">
+                                        <input type="text" id="multicol-username" class="form-control"
+                                            placeholder="password" name="password" {{ isset($user) ? '' : 'required' }}>
+                                    </div>
+                                </div>
 
-                        <div class="pt-4">
-                            <div class="row justify-content-start">
-                                <div class="col-sm-9">
-                                    <button type="submit"
-                                        class="btn btn-primary me-sm-2 me-1 waves-effect waves-light">{{ isset($user) ? 'Update' : 'Submit' }}</button>
-                                    <button class="btn btn-label-secondary waves-effect"><a
-                                            href="{{ route('admin.list') }}">Cancel</a></button>
+                                <div class="pt-4">
+                                    <div class="row justify-content-start">
+                                        <div class="col-sm-9">
+                                            <button type="submit"
+                                                class="btn btn-primary me-sm-2 me-1 waves-effect waves-light">{{ isset($user) ? 'Update' : 'Submit' }}</button>
+                                            <button class="btn btn-label-secondary waves-effect"><a
+                                                    href="{{ route('admin.list') }}">Cancel</a></button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
+    <script>
+        document.getElementById('imageUpload').addEventListener('change', function() {
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(event) {
+                    const imageUrl = event.target.result;
+                    const imagePreview = document.getElementById('imagePreview');
+                    imagePreview.innerHTML = `<div class='d-flex flex-column'><img src="${imageUrl}" alt="Image Preview" style="max-width: 100%; max-height: 200px;">
+                                 <button class='removeButton' id="removeButton">Remove</button></div>`;
 
+                    document.getElementById('removeButton').addEventListener('click', function() {
+                        document.getElementById('imageUpload').value = '';
+                        imagePreview.innerHTML = '';
+                        document.getElementById('uploadButton').style.display = 'inline-block';
+                    });
+                }
+                reader.readAsDataURL(file);
+                document.getElementById('uploadButton').style.display = 'none';
+            }
+        });
+    </script>
 @endsection

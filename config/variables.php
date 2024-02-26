a55161e1870862df8a8f80f764586f470daf791b
arunsai.gandham <arunsai.gandham@karkinos.in>
2024-02-26 21:01:18 +0530

    intial changes

---
+++ config/variables.php
@@ -0,0 +1,9 @@
+<?php
+// Variables
+return [
+  "creatorName" => "Arun Sai Gandham",
+  "creatorUrl" => "",
+  "templateName" => "AVIdols",
+  "templateSuffix" => "Arun Sai Gandham",
+  "templateVersion" => "1.0.0",
+];

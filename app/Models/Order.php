c9cf79a17d35655893059aa733ac92834bf9d25d
Arun Gandham <arun.g@Technovert.com>
2024-02-27 10:04:09 +0530

    Added fillables to models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_id', 'product_id', 'name', 'address', 'phone1', 'phone2', 'price', 'advance', 'cover_price', 'crane_price', 'note', 'model',
+    ];
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name', 'feet', 'price', 'thumbnail', 'images', 'body_color', 'pancha_saree_color', 'type_id', 'created_by', 'model' 
+    ];
 }
